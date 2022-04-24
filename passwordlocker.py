54fdfc80b2eea479e7e5c9efd5347f3489e17796
jacqueline ngabo <ngabo.jay@gmail.com>
2022-04-24 22:06:04 +0300

    add current changes to passwordlocker.py

--- passwordlocker.py
+++ passwordlocker.py
@@ -20,37 +20,37 @@ class User :
        a method to save a new user to the list
        """
 
-#       User.user_list.append(self)
+      User.user_list.append(self)
     
-#     @classmethod
-#     def show_user(cls):
-#         return cls.user_list
+    @classmethod
+    def show_user(cls):
+        return cls.user_list
 
-#     def delete_user(self):
-#       """
-#       method to delete user from  the list
-#       """
-#       User.user_list.remove(self)
+    def delete_user(self):
+      """
+      method to delete user from  the list
+      """
+      User.user_list.remove(self)
 
 
-# class Credentials:
-#     """
-#     Create credentials  class
-#     """
-#     credentials_list = []
-#     def __init__(self,account,name, password):
-#         """
-#         method that defines user credentials to be stored
-#         """
-#         self.account = account
-#         self.name = name
-#         self.password = password
+class Credentials:
+    """
+    Create credentials  class
+    """
+    credentials_list = []
+    def __init__(self,account,name, password):
+        """
+        method that defines user credentials to be stored
+        """
+        self.account = account
+        self.name = name
+        self.password = password
 
-#     def save_datails(self):
-#         """
-#          a method to save the credentials
-#         """
-#         Credentials.credentials_list.append(self)
+    def save_datails(self):
+        """
+         a method to save the credentials
+        """
+        Credentials.credentials_list.append(self)
 
 #     def delete_credentials(self):
 #          """
